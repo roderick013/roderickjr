2270615ed55cfc70c59389ab971b2a5c9ede89e0
krystlkayem <krystalkayem@gmail.com>
2026-02-28 11:24:10 +0800

    newchanges
    okay

--- Program.cs
+++ Program.cs
@@ -4,62 +4,79 @@
     {
         static void Main()
         {
-            string[] students = { "Ubaldo", "Anlatiit", "Redondo", "Sanlorenzo", "Igloso", "Aviles" };
+            Console.WriteLine("=== Attendance Tracker === ");
+            string username = "roderickorfella";
+            string password = "roderick";
 
-            int days = 3;
-            int[,] attendance = new int[students.Length, days];
+            Console.WriteLine("Enter Username: ");
+            string usernameInput = Console.ReadLine();
+            Console.WriteLine("Enter Password: ");
+            string passwordInput = Console.ReadLine();
 
-            int overallPresent = 0;
-            int overallAbsent = 0;
-
-            for (int day = 0; day < days; day++)
+            if (usernameInput == username && passwordInput == password)
             {
-                Console.WriteLine("\n--- Day " + (day + 1) + " Attendance ---");
+                Console.WriteLine("Login successful! Welcomeme, " + username + "!");
 
-                int dailyPresent = 0;
-                int dailyAbsent = 0;
+                string[] students = { "Ubaldo", "Alatiit", "Redondo", "Sanlorenzo", "Igloso", "Aviles" };
 
-                for (int i = 0; i < students.Length; i++)
-                {
-                    Console.Write("Is " + students[i] + " present? (P/A): ");
-                    string status = Console.ReadLine().ToUpper();
+                int days = 3;
+                int[,] attendance = new int[students.Length, days];
 
-                    if (status == "P")
-                    {
-                        attendance[i, day] = 1;
-                        dailyPresent++;
-                    }
-                    else
-                    {
-                        attendance[i, day] = 0;
-                        dailyAbsent++;
-                    }
-                }
-
-                Console.WriteLine("Day " + (day + 1) + " Present: " + dailyPresent);
-                Console.WriteLine("Day " + (day + 1) + " Absent: " + dailyAbsent);
-
-                overallPresent += dailyPresent;
-                overallAbsent += dailyAbsent;
-            }
-
-            Console.WriteLine("\n=== Each Student Total Attendance ===");
-
-            for (int i = 0; i < students.Length; i++)
-            {
-                int studentTotal = 0;
+                int overallPresent = 0;
+                int overallAbsent = 0;
 
                 for (int day = 0; day < days; day++)
                 {
-                    studentTotal += attendance[i, day];
+                    Console.WriteLine("\n--- Day " + (day + 1) + " Attendance ---");
+
+                    int dailyPresent = 0;
+                    int dailyAbsent = 0;
+
+                    for (int i = 0; i < students.Length; i++)
+                    {
+                        Console.Write("Is " + students[i] + " present? (P/A): ");
+                        string status = Console.ReadLine().ToUpper();
+
+                        if (status == "P")
+                        {
+                            attendance[i, day] = 1;
+                            dailyPresent++;
+                        }
+                        else
+                        {
+                            attendance[i, day] = 0;
+                            dailyAbsent++;
+                        }
+                    }
+
+                    Console.WriteLine("Day " + (day + 1) + " Present: " + dailyPresent);
+                    Console.WriteLine("Day " + (day + 1) + " Absent: " + dailyAbsent);
+
+                    overallPresent += dailyPresent;
+                    overallAbsent += dailyAbsent;
                 }
 
-                Console.WriteLine(students[i] + " was present for " + studentTotal + " out of " + days + " days.");
+                Console.WriteLine("\n=== Each Student Total Attendance ===");
+
+                for (int i = 0; i < students.Length; i++)
+                {
+                    int studentTotal = 0;
+
+                    for (int day = 0; day < days; day++)
+                    {
+                        studentTotal += attendance[i, day];
+                    }
+
+                    Console.WriteLine(students[i] + " was present for " + studentTotal + " out of " + days + " days.");
+                }
+
             }
 
-
-
-
-
+            else
+            {
+                Console.WriteLine("Invalid username or Password. Access denied. ");
+                return;
+            }
         }
     }
+}
